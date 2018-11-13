6561e937ea20f72bd80622dd2de0771030b321db
Nishil <nishil.shah17@gmail.com>
2018-11-12 22:12:44 -0600

    init package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyff',
+    version='0.0.1',
+    description='an extensive fantasy football library (ESPN API wrapper and more)',
+    license='Apache 2.0',
+    packages=['pyff'],
+    author='Nishil Shah, Arvind Vijayakumar',
+    author_email='nishilshah17@gmail.com, avjykmr2@illinois.edu',
+    keywords=['fantasy', 'football', 'espn'],
+    url='https://github.com/nishilshah17/PyFF'
+)
